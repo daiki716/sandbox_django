80a725ca7e1e8ac38d14978c84e3ab5ad8b9b580
student <student@o-hara-kashiwa.local>
2020-09-24 15:03:02 +0900

    メール送信

--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView
 from .forms import Inquiryform
 
@@ -9,3 +10,8 @@ class IndexView(TemplateView):
 class InquiryView(FormView):
     template_name = 'inquiry.html'
     form_class = Inquiryform
+    success_url = reverse_lazy('diary:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        return super().form_valid(form)
